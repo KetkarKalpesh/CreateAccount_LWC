8a1ee19d6a9a5428a7da87d399262d02858206e3
Kalpesh Ketkar <106992078+KetkarKalpesh@users.noreply.github.com>
2022-09-13 12:02:58 +0530

    Add files via upload

---
+++ craeteAccount.js
@@ -0,0 +1,31 @@
+import { LightningElement } from 'lwc';
+import ACCOUNT_NAME from '@salesforce/schema/Account.Name';
+import ACCOUNT_PHONE from '@salesforce/schema/Account.Phone';
+import ACCOUNT_TYPE from '@salesforce/schema/Account.Type';
+import Account_RATING from '@salesforce/schema/Account.Rating';
+import ACCOUNT_INDUSTRY from '@salesforce/schema/Account.Industry';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class CraeteAccount extends LightningElement {
+
+    objectApiName = 'Account';
+    fieldList = [ACCOUNT_NAME, ACCOUNT_PHONE, ACCOUNT_TYPE, Account_RATING, ACCOUNT_INDUSTRY];
+    handleAccountCreate(event){
+        const evt = new ShowToastEvent({
+            title : "Account Created",
+            message : "Record ID: " + event.detail.id,
+            variant : "success",
+        });
+        this.dispatchEvent(evt);
+        // this[NavigationMixin.Navigate]({
+        //     type: 'standard__recordPage',
+        //     attributes: {
+        //         recordId: event.detail.id,
+        //         objectApiName: 'Account',
+        //         actionName: 'view'
+        //     },
+        // });
+    }
+
+}
